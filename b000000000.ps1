fc1340b136928fab1b0dab2c6a9a9db2c6470b71
Brice <62551735+b300098957@users.noreply.github.com>
2020-04-01 20:47:11 -0400

    Update b000000000.ps1

--- b000000000.ps1
+++ b000000000.ps1
@@ -1,1 +1,29 @@
+<#
+.SYNOPSIS
+    This script/function does - What?
+
+.DESCRIPTION
+    A more detailed description of why and how the function works.
+
+.PARAMETER DemoParam1
+    The parameter DemoParam1 is used to define the value of blah and also blah.
+
+.PARAMETER DemoParam2
+    The parameter DemoParam2 is used to define the value of blah and also blah.
+
+.EXAMPLE
+    The example below does blah
+    PS C:\> Example
+
+.EXAMPLE
+    Another example
+
+.NOTES
+    Author: Name
+    Last Edit: yyyy-mm-dd
+    Version 1.0 - initial release of blah
+    Version 1.1 - update for blah
+
+#>
+
 Write-Host "Hello World"
