ee454ec421c76ee3366d2a42a340fb0ad0d919fc
Etienne Tochgaly <etiennetochgaly@gmail.com>
2020-04-02 20:44:22 -0400

    0

--- b000000000-structure.ps1
+++ b000000000-structure.ps1
@@ -11,6 +11,24 @@
 
 #>
 
-$personneNom = "Alice"
-$personneAge = 35
-Write-Host "Bonjour $personneNom, tu as $personneAge ans. "
+[String]$personneNom = "Alice"
+[Int]$personneAge = 35
+
+
+# Test sur l'age de la personne
+switch ($personneAge) {
+    {$_ -le 25} {Write-Host 'en pleine jeunesse'; break }
+    {$_ -le 35} {Write-Host 'en pleine force vive'; break }
+    {$_ -le 45} {Write-Host 'en pleine maturité'; break }
+    Default {Write-Host 'en pleine serénité'}
+}
+
+$villes = "Toronto","Mississauga","Scarborough","Brampton"
+
+for ($i = 0; $i -le $villes.Count; $i = $i + 1) {
+    $villes[$i]
+}
+
+Clear-Host
+
+"{0}" -F ($villes.Count % 2)
